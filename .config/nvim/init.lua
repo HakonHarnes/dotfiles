37e5733c8e4f15750cffa8f45bb030695e784f2f
Håkon Harnes <hakon@Hakons-MacBook-Pro.local>
2024-12-07 14:51:24 +0100

    feat: add cwd autocmd

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,16 @@
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
 
+local group_cdpwd = vim.api.nvim_create_augroup("group_cdpwd", { clear = true })
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = group_cdpwd,
+  pattern = "*",
+  callback = function()
+    local path = vim.fn.expand("%:p:h"):gsub("oil://", "")
+    vim.api.nvim_set_current_dir(path)
+  end,
+})
+
 require("config.lazy")
 require("config.dev")
 require("config.statusbar")
